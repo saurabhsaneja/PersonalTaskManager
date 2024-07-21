6b7f8752d79e4dba833dea439a455cd652e5454c
Saurabh Saneja <saurabh250183@gmail.com>
2024-07-21 06:05:34 -0700

    adds button animation

--- src/components/MyButton.tsx
+++ src/components/MyButton.tsx
@@ -10,18 +10,43 @@ import {
     TouchableOpacity,
     Image
 } from 'react-native';
+import Animated, { Easing, useSharedValue, useAnimatedStyle, withSpring } from 'react-native-reanimated';
 
 interface Props {
     title: string;
     onPress: any;
     style?: any;
 }
+const AnimatedTouchableOpacity = Animated.createAnimatedComponent(TouchableOpacity);
+
 const MyButton = ({ title, onPress, style = {} }: Props) => {
+    // Create a shared value for the animation
+    const scale = useSharedValue(1);
+
+    // Define the animated style
+    const animatedStyle = useAnimatedStyle(() => {
+        return {
+            transform: [{ scale: scale.value }],
+        };
+    });
+
+    // Handle the button press
+    const handlePressIn = () => {
+        scale.value = withSpring(0.3, { easing: Easing.out(Easing.exp) });
+    };
+
+    const handlePressOut = () => {
+        scale.value = withSpring(1, { easing: Easing.out(Easing.exp) });
+    };
 
     return (
-        <TouchableOpacity onPress={onPress} style={[styles.button, style]} >
+        <AnimatedTouchableOpacity
+            onPressIn={handlePressIn}
+            onPressOut={handlePressOut}
+            onPress={onPress} style={[styles.button, style, animatedStyle]}>
             <Text style={styles.someText}>{title}</Text>
-        </TouchableOpacity>
+        </AnimatedTouchableOpacity>
+
     )
 }
 
